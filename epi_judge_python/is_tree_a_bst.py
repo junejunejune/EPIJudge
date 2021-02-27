2fef3ae8fd298e77acadcc1af5c4e449647d3812
hy00nc <chohy0555@gmail.com>
2021-02-27 07:35:35 +0000

    14.1: is_tree_a_bst, inefficient initial verw

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -1,10 +1,29 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
+from test_framework.binary_tree_utils import generate_preorder
 
 
 def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    if tree is None:
+        return True
+
+    if tree.left:
+        if tree.data < tree.left.data:
+            return False
+        left_sub = generate_preorder(tree.left)
+        if max(left_sub) > tree.data:
+            return False
+    if tree.right:
+        if tree.data > tree.right.data:
+            return False
+        right_sub = generate_preorder(tree.right)
+        if min(right_sub) < tree.data:
+            return False
+
+    left = is_binary_tree_bst(tree.left)
+    right = is_binary_tree_bst(tree.right)
+
+    return left and right
 
 
 if __name__ == '__main__':
