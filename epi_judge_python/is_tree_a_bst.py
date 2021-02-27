afd89bd22011d551ede6721729add29d03ff0498
junejunejune <je2156@gmail.com>
2021-02-27 23:36:32 +0900

    0227

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -2,10 +2,20 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
-def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
 
+#def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
+    # TODO - you fill in here.
+
+def is_binary_tree_bst(tree, r=[float('-inf'), float('inf')]):
+    if not tree:
+        return True
+    if not r[0] <= tree.data <= r[1]:
+        return False
+
+    return is_binary_tree_bst(tree.left, [r[0], tree.data]) and is_binary_tree_bst(tree.right, [tree.data, r[1]])
+
+
+    #return is_binary_tree_bst(tree.right) and is_binary_tree_bst(tree.left)
 
 if __name__ == '__main__':
     exit(
--- epi_judge_python/search_first_greater_value_in_bst.py
+++ epi_judge_python/search_first_greater_value_in_bst.py
@@ -4,13 +4,27 @@ from bst_node import BstNode
 from test_framework import generic_test
 
 
-def find_first_greater_than_k(tree: BstNode, k: int) -> Optional[BstNode]:
+def find_first_greater_than_k(tree: BstNode, k: int, currMinNode) -> Optional[BstNode]:
     # TODO - you fill in here.
-    return None
+    # INORDER: left->root->right
+    if not tree:
+        return currMinNode
+
+    #currMinNode 업데이트
+    if tree.data > k and (not currMinNode or tree.data <= currMinNode.data):
+        currMinNode = tree
+
+    if tree.data > k:
+        #left에 더k와 가깝게 큰 것 있나 봐야 됨
+        #만약 없으면 현재 노트가 답
+        return find_first_greater_than_k(tree.left, k, currMinNode)
+    else:
+        #left subtree와 root는 아니므로, right subtree를 봐야함
+        return find_first_greater_than_k(tree.right, k, currMinNode)
 
 
 def find_first_greater_than_k_wrapper(tree, k):
-    result = find_first_greater_than_k(tree, k)
+    result = find_first_greater_than_k(tree, k, None)
     return result.data if result else -1
 
 
